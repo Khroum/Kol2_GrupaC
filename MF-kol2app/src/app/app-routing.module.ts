9ee6d54b103cff4eac454f07e9f4e9813af02114
Khroum <fladro224@gmail.com>
2020-06-19 12:40:27 +0200

    final

--- MF-kol2app/src/app/app-routing.module.ts
+++ MF-kol2app/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShopDetailsMFComponent } from './components/shop-details-mf/shop-details-mf.component';
 
-const routes: Routes = [{ path: '/:id', component: ShopDetailsMFComponent }];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MF-kol2app/src/app/components/shop-details-mf/shop-details-mf.component.ts
+++ MF-kol2app/src/app/components/shop-details-mf/shop-details-mf.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-shop-details-mf',
   templateUrl: './shop-details-mf.component.html',
-  styleUrls: ['./shop-details-mf.component.css']
+  styleUrls: ['./shop-details-mf.component.css'],
 })
 export class ShopDetailsMFComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
